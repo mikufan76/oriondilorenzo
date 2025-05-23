a4a8f5abfaa33b242ee8adfd54c63ee264cc1ae2
mikufan76 <56976587+mikufan76@users.noreply.github.com>
2025-05-22 21:08:03 -0400

    Responsive header (text)
    Incomplete as the text doesnt change size just yet

--- oriondilorenzo/components/shared/Header.tsx
+++ oriondilorenzo/components/shared/Header.tsx
@@ -11,15 +11,21 @@ export function Header(props: HeaderProps) {
   return (
     <div className="w-full h-screen bg-[url(/header-bg.png)] bg-cover flex justify-center items-center">
       {/* Foreground */}
-      <div className="w-[90%] h-[90%] flex flex-col text-center bg-bg text-primary p-2">
-        {/** Title plate */}
-        <div className="flex flex-col w-2/3 min-w-[250px] h-min">
-          <Name className="w-5/6 min-w-[208px] translate-y-[75%] p-1" />
-          <div className="h-1/3 overflow-visible">
-          <StarBar />
+      <div className="w-[90%] h-[90%] min-w-[300px] flex flex-col text-center bg-bg text-primary p-2">
+        {/* Left Side */}
+        <div className="h-full w-[62.5%] relative">
+          {/** Title plate */}
+          <div className="flex flex-col w-full min-w-[300px] h-2/5 border-2 border-green">
+            <Name className="w-5/6 min-w-[220px] p-1" />
+            <div className="h-1/5 md:h-2/5 overflow-visible border-blue border-2 translate-y-[-45%]">
+              <StarBar />
+            </div>
+            <div className="min-w-[300px] cascadia-code border-red border-2 text-wrap text-left w-full translate-y-[-205%] md:translate-y-[-405%]">
+              Fullstack Developer
+            </div>
           </div>
+          <Logo className="h-1/2 w-min animate-in fade-in" />
         </div>
-        <Logo className="h-1/2 w-min animate-in fade-in" />
       </div>
     </div>
   )
