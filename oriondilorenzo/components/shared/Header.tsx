7a815a2c4eb4fc83722d7683a5e7651e4df84a4f
mikufan76 <56976587+mikufan76@users.noreply.github.com>
2025-05-24 13:45:45 -0400

    Fix layout

--- oriondilorenzo/components/shared/Header.tsx
+++ oriondilorenzo/components/shared/Header.tsx
@@ -9,7 +9,7 @@ export function Header(props: HeaderProps) {
       {/* Foreground */}
       <div className="w-[90%] h-[90%] min-w-[300px] flex flex-row text-center bg-bg text-primary px-2 py-4">
         {/* Left Side */}
-        <div className="h-full w-full md:px-0 md:w-[62.5%] relative border-yellow border-2">
+        <div className="h-full w-full md:px-0 sm:w-[62.5%] relative border-yellow border-2">
           <TitlePlate />
           <div className="logo-wrapper h-1/2 w-full border-2 border-blue flex justify-center items-center ">
             <Logo className="h-full w-min animate-in fade-in border-2 border-red rotate-[-10deg] min-w-[300px] scale-125" />
